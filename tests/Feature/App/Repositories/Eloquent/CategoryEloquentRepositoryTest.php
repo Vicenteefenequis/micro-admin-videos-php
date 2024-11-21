85007dd51bc5ed9ea2bacb271af59fc52d98217c
Vicente <vicente19981@live.com>
2024-11-21 15:08:23 -0300

    test: garantindo que categoria e recuperada pelo id

--- tests/Feature/App/Repositories/Eloquent/CategoryEloquentRepositoryTest.php
+++ tests/Feature/App/Repositories/Eloquent/CategoryEloquentRepositoryTest.php
@@ -5,26 +5,52 @@ namespace Tests\Feature\App\Repositories\Eloquent;
 use App\Models\Category as Model;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
 use Core\Domain\Entity\Category as EntityCategory;
+use Core\Domain\Exception\NotFoundException;
 use Core\Domain\Repository\CategoryRepositoryInterface;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Throwable;
 
 class CategoryEloquentRepositoryTest extends TestCase
 {
+    protected CategoryEloquentRepository $repository;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->repository = new CategoryEloquentRepository(new Model());
+
+    }
+
     public function testInsert()
     {
-        $repository = new CategoryEloquentRepository(new Model());
-
         $entity = new EntityCategory(
             name: 'teste'
         );
-        $response = $repository->insert($entity);
+        $response = $this->repository->insert($entity);
 
-        $this->assertInstanceOf(CategoryRepositoryInterface::class, $repository);
+        $this->assertInstanceOf(CategoryRepositoryInterface::class, $this->repository);
         $this->assertInstanceOf(EntityCategory::class, $response);
         $this->assertDatabaseHas('categories', [
             'name' => $entity->name
         ]);
+    }
+
+    public function testFindById()
+    {
+        $category = Model::factory()->create();
+        $response = $this->repository->findById($category->id);
+
+        $this->assertInstanceOf(EntityCategory::class, $response);
+        $this->assertEquals($category->id, $response->id());
+    }
+
+    public function testFindByIdNotFound()
+    {
+        try {
+            $this->repository->findById('fake_id');
+            $this->assertTrue(false);
+        } catch (Throwable $th) {
+            $this->assertInstanceOf(NotFoundException::class, $th);
+        }
     }
 }
