2b2ecf793b7a47b549eab3ce7cff3cf5a67af839
Vicente <vicente19981@live.com>
2024-11-21 22:45:52 -0300

    Test Integration Controller Update Category

--- tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/App/Http/Controllers/Api/CategoryControllerTest.php
@@ -4,11 +4,13 @@ namespace Tests\Feature\App\Http\Controllers\Api;
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category as Model;
 use App\Repositories\Eloquent\CategoryEloquentRepository;
 use Core\UseCase\Category\CreateCategoryUseCase;
 use Core\UseCase\Category\ListCategoriesUseCase;
 use Core\UseCase\Category\ListCategoryUseCase;
+use Core\UseCase\Category\UpdateCategoryUseCase;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -59,6 +61,25 @@ class CategoryControllerTest extends TestCase
             useCase: new ListCategoryUseCase($this->repository),
             id: $category->id,
         );
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(ResponseAlias::HTTP_OK, $response->getStatusCode());
+    }
+
+    public function test_update()
+    {
+        $category = Model::factory()->create();
+        $request = new UpdateCategoryRequest();
+        $request->headers->set('Content-Type', 'application/json');
+        $request->setJson(new ParameterBag([
+            'name' => 'Updated',
+        ]));
+        $response = $this->controller->update(
+            request: $request,
+            useCase: new UpdateCategoryUseCase($this->repository),
+            id: $category->id,
+        );
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
         $this->assertEquals(ResponseAlias::HTTP_OK, $response->getStatusCode());
     }
 }
