f5064d07569035a8272194aea33d1625c7882416
Vicente <vicente19981@live.com>
2024-11-23 13:56:48 -0300

    Fix tests broken include currentPage on list

--- tests/Unit/App/Http/Controllers/Api/CategoryControllerUnitTest.php
+++ tests/Unit/App/Http/Controllers/Api/CategoryControllerUnitTest.php
@@ -22,7 +22,7 @@ class CategoryControllerUnitTest extends TestCase
         $mockRequest->shouldReceive('get')->andReturn('teste');
 
         $mockDtoOutput = Mockery::mock(ListCategoriesOutputDto::class, [
-            [], 1, 1, 1, 1, 1, 1
+            [], 1, 1, 1, 1, 1, 1, 1
         ]);
 
         $mockUseCase = Mockery::mock(ListCategoriesUseCase::class);
