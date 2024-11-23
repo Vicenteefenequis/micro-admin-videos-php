c3fb50b5f5dcf45fdf00cfe70b1e1f2b26be13ba
Vicente <vicente19981@live.com>
2024-11-23 12:20:06 -0300

    Feat Registration exceptions error on handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,11 @@
 
 namespace App\Exceptions;
 
+use Core\Domain\Exception\EntityValidationException;
+use Core\Domain\Exception\NotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -38,4 +42,24 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof NotFoundException) {
+            return $this->showError($exception->getMessage(), ResponseAlias::HTTP_NOT_FOUND);
+        }
+
+        if ($exception instanceof EntityValidationException) {
+            return $this->showError($exception->getMessage(), ResponseAlias::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return parent::render($request, $exception);
+    }
+
+    private function showError(string $message, int $statusCode): JsonResponse
+    {
+        return response()->json([
+            'message' => $message
+        ], $statusCode);
+    }
+
 }
