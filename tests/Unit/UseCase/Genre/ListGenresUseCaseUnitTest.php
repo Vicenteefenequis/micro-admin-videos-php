ec32247e99452a91f5bae8e12852f55d675d7bb4
Vicente <vicente19981@live.com>
2024-11-26 19:57:20 -0300

    Unit Test ensure List Genre Use Case

---
+++ tests/Unit/UseCase/Genre/ListGenresUseCaseUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Unit\UseCase\Genre;
+
+use Core\Domain\Repository\PaginationInterface;
+use Core\UseCase\DTO\Genre\List\{
+    ListGenresInputDto,
+    ListGenresOutputDto
+};
+use Core\Domain\Repository\GenreRepositoryInterface;
+use Core\UseCase\Genre\ListGenresUseCase;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class ListGenresUseCaseUnitTest extends TestCase
+{
+    public function test_usecase()
+    {
+        $mockRepo = Mockery::mock(stdClass::class, GenreRepositoryInterface::class);
+        $mockRepo->shouldReceive('paginate')->andReturn($this->mockPagination());
+
+        $mockDtoInput = Mockery::mock(ListGenresInputDto::class, [
+            'teste',
+            'desc',
+            1,
+            15
+        ]);
+
+        $useCase = new ListGenresUseCase($mockRepo);
+        $response = $useCase->execute($mockDtoInput);
+
+        $this->assertInstanceOf(ListGenresOutputDto::class, $response);
+        Mockery::close();
+    }
+
+    protected function mockPagination(array $items = [])
+    {
+        $this->mockPagination = Mockery::mock(stdClass::class, PaginationInterface::class);
+        $this->mockPagination->shouldReceive('items')->andReturn($items);
+        $this->mockPagination->shouldReceive('total')->andReturn(0);
+        $this->mockPagination->shouldReceive('currentPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('firstPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('lastPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('perPage')->andReturn(0);
+        $this->mockPagination->shouldReceive('to')->andReturn(0);
+        $this->mockPagination->shouldReceive('from')->andReturn(0);
+
+        return $this->mockPagination;
+    }
+}
