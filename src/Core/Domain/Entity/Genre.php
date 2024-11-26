b8dd334678fedf1fc9c6e6c0e552017a6ec06947
Vicente <vicente19981@live.com>
2024-11-26 12:40:57 -0300

    Feat ensure attributes of Genre Domain

---
+++ src/Core/Domain/Entity/Genre.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+use Core\Domain\Entity\Traits\MethodsMagicsTrait;
+use Core\Domain\ValueObject\Uuid;
+use DateTime;
+
+class Genre
+{
+
+    use MethodsMagicsTrait;
+
+    public function __construct(
+        protected string $name,
+        protected ?Uuid  $id = null,
+        protected bool $isActive = true,
+        protected ?DateTime $createdAt = null,
+    )
+    {
+        $this->id ??= Uuid::random();
+        $this->createdAt ??= new DateTime();
+    }
+
+}
