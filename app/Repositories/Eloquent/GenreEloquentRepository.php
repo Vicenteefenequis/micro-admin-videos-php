0c7ec016f56e39adbea9f1b1bdad278da07ef79f
Vicente <vicente19981@live.com>
2024-12-03 12:35:13 -0300

    Feat ensure GenreRepository update genres

--- app/Repositories/Eloquent/GenreEloquentRepository.php
+++ app/Repositories/Eloquent/GenreEloquentRepository.php
@@ -72,7 +72,19 @@ class GenreEloquentRepository implements GenreRepositoryInterface
 
     public function update(Entity $genre): Entity
     {
-        // TODO: Implement update() method.
+        if (!$genreDb = $this->model->find($genre->id)) {
+            throw new NotFoundException("Genre {$genre->id} not found");
+        }
+
+        $genreDb->update([
+            'name' => $genre->name
+        ]);
+
+        $genreDb->refresh();
+
+        return $this->toGenre($genreDb);
+
+
     }
 
     public function delete(string $genreId): bool
