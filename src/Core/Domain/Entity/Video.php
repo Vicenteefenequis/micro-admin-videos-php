a735894654abb5b763fe2d41a743c6425943a035
Vicente <vicente19981@live.com>
2024-12-05 16:54:04 -0300

    Feat ensure video entity correctly attributes

---
+++ src/Core/Domain/Entity/Video.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Domain\Entity;
+
+use Core\Domain\Entity\Traits\MethodsMagicsTrait;
+use Core\Domain\Enum\Rating;
+use Core\Domain\ValueObject\Uuid;
+
+class Video
+{
+    use MethodsMagicsTrait;
+
+    public function __construct(
+        protected string $title,
+        protected string $description,
+        protected int    $yearLaunched,
+        protected int    $duration,
+        protected bool   $opened,
+        protected Rating $rating,
+        protected ?Uuid  $id = null,
+        protected bool   $published = false,
+    )
+    {
+        $this->id ??= Uuid::random();
+    }
+
+}
