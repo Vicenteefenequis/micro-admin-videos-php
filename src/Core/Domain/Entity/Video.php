77d219e300d06717b5538cd489ce4e2acd38bce7
Vicente <vicente19981@live.com>
2024-12-05 18:49:07 -0300

    Feat ensure trailer file path on video

--- src/Core/Domain/Entity/Video.php
+++ src/Core/Domain/Entity/Video.php
@@ -5,6 +5,7 @@ namespace Core\Domain\Entity;
 use Core\Domain\Entity\Traits\MethodsMagicsTrait;
 use Core\Domain\Enum\Rating;
 use Core\Domain\ValueObject\Image;
+use Core\Domain\ValueObject\Media;
 use Core\Domain\ValueObject\Uuid;
 use DateTime;
 
@@ -27,51 +28,66 @@ class Video
         protected bool      $published = false,
         protected ?DateTime $createdAt = null,
         protected ?Image    $thumbFile = null,
-        protected ?Image    $thumbHalf = null
+        protected ?Image    $thumbHalf = null,
+        protected ?Media    $trailerFile = null,
     )
     {
         $this->id ??= Uuid::random();
         $this->createdAt ??= new DateTime();
     }
 
-    public function addCategoryId(string $categoryId)
+    public
+    function addCategoryId(string $categoryId)
     {
         $this->categoriesId[] = $categoryId;
     }
 
-    public function removeCategoryId(string $categoryId)
+    public
+    function removeCategoryId(string $categoryId)
     {
         array_splice($this->categoriesId, array_search($categoryId, $this->categoriesId), 1);
     }
 
-    public function addGenreId(string $genreId)
+    public
+    function addGenreId(string $genreId)
     {
         $this->genresId[] = $genreId;
     }
 
-    public function removeGenreId(string $genreId)
+    public
+    function removeGenreId(string $genreId)
     {
         array_splice($this->genresId, array_search($genreId, $this->genresId), 1);
     }
 
-    public function addCastMemberId(string $castMemberId)
+    public
+    function addCastMemberId(string $castMemberId)
     {
         $this->castMembersId[] = $castMemberId;
     }
 
-    public function removeCastMemberId(string $castMemberId)
+    public
+    function removeCastMemberId(string $castMemberId)
     {
         $this->castMembersId = array_diff($this->castMembersId, [$castMemberId]);
     }
 
-    public function thumbFile(): ?Image
+    public
+    function thumbFile(): ?Image
     {
         return $this->thumbFile;
     }
 
-    public function thumbHalf(): ?Image
+    public
+    function thumbHalf(): ?Image
     {
         return $this->thumbHalf;
+    }
+
+    public
+    function trailerFile(): ?Media
+    {
+        return $this->trailerFile;
     }
 
 }
