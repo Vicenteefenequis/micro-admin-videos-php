0b621402db52ef2024ca9efbf412154b2a79af22
Vicente <vicente19981@live.com>
2024-12-05 18:29:10 -0300

    Feat ensure create VO for imagem on video

--- src/Core/Domain/Entity/Video.php
+++ src/Core/Domain/Entity/Video.php
@@ -4,6 +4,7 @@ namespace Core\Domain\Entity;
 
 use Core\Domain\Entity\Traits\MethodsMagicsTrait;
 use Core\Domain\Enum\Rating;
+use Core\Domain\ValueObject\Image;
 use Core\Domain\ValueObject\Uuid;
 use DateTime;
 
@@ -25,6 +26,7 @@ class Video
         protected ?Uuid     $id = null,
         protected bool      $published = false,
         protected ?DateTime $createdAt = null,
+        protected ?Image    $thumbFile = null
     )
     {
         $this->id ??= Uuid::random();
@@ -59,6 +61,11 @@ class Video
     public function removeCastMemberId(string $castMemberId)
     {
         $this->castMembersId = array_diff($this->castMembersId, [$castMemberId]);
+    }
+
+    public function thumbFile(): ?Image
+    {
+        return $this->thumbFile;
     }
 
 }
