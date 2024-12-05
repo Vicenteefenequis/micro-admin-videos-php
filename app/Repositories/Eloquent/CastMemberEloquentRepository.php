7ab0bbd507b55eafa55d32d60d658206322399ba
Vicente <vicente19981@live.com>
2024-12-05 10:52:50 -0300

    Feat ensure CastMemberRepository insert a member

---
+++ app/Repositories/Eloquent/CastMemberEloquentRepository.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\CastMember as Model;
+use Carbon\Traits\Cast;
+use Core\Domain\Entity\CastMember;
+use Core\Domain\Enum\CastMemberType;
+use Core\Domain\Repository\CastMemberRepositoryInterface;
+use Core\Domain\Repository\PaginationInterface;
+use Core\Domain\ValueObject\Uuid;
+use DateTime;
+
+class CastMemberEloquentRepository implements CastMemberRepositoryInterface
+{
+
+    public function __construct(protected Model $model)
+    {
+    }
+
+    public function insert(CastMember $castMember): CastMember
+    {
+        $castMemberDb = $this->model->create([
+            'id' => $castMember->id(),
+            'name' => $castMember->name,
+            'type' => $castMember->type->value,
+            'created_at' => $castMember->createdAt(),
+        ]);
+
+        return $this->toCastMember($castMemberDb);
+    }
+
+    public function findById(string $castMemberId): CastMember
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function findAll(string $filter = '', $order = 'DESC'): array
+    {
+        // TODO: Implement findAll() method.
+    }
+
+    public function paginate(string $filter = '', $order = 'DESC', int $page = 1, int $totalPage = 15): PaginationInterface
+    {
+        // TODO: Implement paginate() method.
+    }
+
+    public function update(CastMember $castMember): CastMember
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete(string $castMemberId): bool
+    {
+        // TODO: Implement delete() method.
+    }
+
+    private function toCastMember(object $object): CastMember
+    {
+        $entity = new CastMember(
+            name: $object->name,
+            type: CastMemberType::from($object->type),
+            id: new Uuid($object->id),
+            createdAt: new DateTime($object->created_at),
+        );
+
+
+        return $entity;
+    }
+}
