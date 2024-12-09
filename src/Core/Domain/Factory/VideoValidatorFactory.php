3e06249560ea3676d92dcb1a131e85f7b70e494b
Vicente <vicente19981@live.com>
2024-12-09 16:44:37 -0300

    feat: create rakit validator and using in video validator factory

--- src/Core/Domain/Factory/VideoValidatorFactory.php
+++ src/Core/Domain/Factory/VideoValidatorFactory.php
@@ -2,15 +2,13 @@
 
 namespace Core\Domain\Factory;
 
-use Core\Domain\Validation\{
-    ValidatorInterface,
-    VideoLaravelValidation
-};
+use Core\Domain\Validation\{ValidatorInterface, VideoRakitValidator};
+
 class VideoValidatorFactory
 {
     public static function create(): ValidatorInterface
     {
-        return new VideoLaravelValidation();
+        return new VideoRakitValidator();
     }
 
 }
